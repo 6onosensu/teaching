979558d6327fa4bea1465d1a1454482e5bdf0833
Dasha <darja.portnova@gmail.com>
2024-03-10 15:38:06 +0200

    ljaguha

---
+++ les1/input_print.py
@@ -0,0 +1,56 @@
+print("""
+      @..@
+     (----)
+     (\__/)
+    ^^ "" ^^""")
+
+
+first = 20
+second = 30
+sum = first + second
+print(sum)
+
+a = '20'
+b = '30'
+sum2 = a + b
+print(sum2)
+
+# i = input('Hello! What is your name?: ')
+# print('Hello, ' + i + '!')
+
+
+print(type(a))
+
+c = a == b
+print(c)
+
+a = int(input('a: '))
+b = float(input('b: '))
+print(a / b)
+
+
+hastag = '#'
+multiply = hastag * 4
+print(multiply)
+
+txt = 'There are 10 candies on the table!'
+enter = input(txt)
+enter2 = input('There are 10 candies on the table!' + txt)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
